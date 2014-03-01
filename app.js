c7b9b0971775a9c6aa5c0280304affd8fa02ab9d
astrobolidos <lisandroalavez@gmail.com>
2014-03-01 22:48:03 +0000

    added ng-keypress to search controller

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-var app = angular.module('kkcounter', ['ngAnimate'])
-.controller('MainCtrl', ['$scope', function ($scope) {
-    $scope.text = 'lisandro';
+var app = angular.module('kkcounter', ['ngAnimate']);
+
+app.controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.text = [];
+
+    $scope.search = function() {
+    	console.log('you have clicked the search');
+    };
+
+    $scope.searching = function(evt) {
+    	console.log( evt );
+    };
 }]);
